7e23da890b7fcef91d122dc124c418780fad5f22
idc777 <idcdcdc.@163com>
2022-03-04 21:21:30 +0800

    update

--- Draft/test.cpp
+++ Draft/test.cpp
@@ -1,7 +1,10 @@
+
 #include <stdio.h>
+#include <iostream>
 int main(){
    char name[100];
    printf("What is your name?\n");
+   std::cout << "It's Cpp code\n";
    scanf("%s",name);
    printf("Hello,%s,nice to meet you!\n",name);
 }
